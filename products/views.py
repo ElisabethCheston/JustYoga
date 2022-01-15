8860881e902d16b8cb46f6a8072fabd7e74cbc2f
ElisabethCheston <elisabeth.cheston@gmail.com>
2022-01-15 16:03:21 +0000

    Add html and url to view products.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def products(request):
+    # Show all products, including sorting and search queries
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
